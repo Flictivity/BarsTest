527d533c9cf359ed26d5f55839b9ce07a013cef8
Flictivity <85502952+Flictivity@users.noreply.github.com>
2022-03-21 21:36:25 +0300

    Add files via upload

---
+++ Event/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Event_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass test = new SomeClass();
+
+            test.OneKeyPressed += Print;
+            test.Run();
+        }
+
+        private static void Print(object sender, char ch)
+        {
+            Console.WriteLine(ch);
+        }
+    }
+}
---
+++ Event/SomeClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Event_Delegate
+{
+    class SomeClass
+    {
+        public event EventHandler<char> OneKeyPressed;
+
+        public void Run()
+        {
+            var inputStr = Console.ReadKey().KeyChar;
+            while (inputStr != 'c')
+            {
+                OneKeyPressed?.Invoke(this, inputStr);
+
+                inputStr = Console.ReadKey().KeyChar; 
+            }
+        }
+    }
+}
