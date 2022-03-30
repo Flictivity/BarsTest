050e599c717d11d34b951a06c8d8576e968c35b6
Flictivity <85502952+Flictivity@users.noreply.github.com>
2022-03-30 22:23:13 +0300

    Add files via upload

---
+++ Generics/LocalFileLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ILogger
+{
+    public class LocalFileLogger<T> : ILogger
+    {
+        private string _path;
+
+        public LocalFileLogger(string path)
+        {
+            _path = path;
+        }
+
+        public void LogInfo(string message)
+        {
+            File.AppendAllText(Path, $"[Info]: [{typeof(T).Name}] : {message}\n");
+        }
+
+        public void LogWarning(string message)
+        {
+            File.AppendAllText(Path, $"[Warning] : [{typeof(T).Name}] : {message}\n");
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            File.AppendAllText(Path, $"[Error] : [{typeof(T).Name}] : {message}. {ex.Message}\n");
+        }
+    }
+}
+
